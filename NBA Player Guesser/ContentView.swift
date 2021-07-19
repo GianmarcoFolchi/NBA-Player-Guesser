a12a8349378b239e52fcd65b405f6d050d6b468a
Gianmarco Folchi <gmfolc@gmail.com>
2021-07-19 01:21:51 -0400

    Initial Commit

---
+++ NBA Player Guesser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Player Guesser
+//
+//  Created by Gianmarco Folchi on 7/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBA Player GuesserTests/NBA_Player_GuesserTests.swift
@@ -0,0 +1,33 @@
+//
+//  NBA_Player_GuesserTests.swift
+//  NBA Player GuesserTests
+//
+//  Created by Gianmarco Folchi on 7/19/21.
+//
+
+import XCTest
+@testable import NBA_Player_Guesser
+
+class NBA_Player_GuesserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
